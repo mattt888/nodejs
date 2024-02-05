94d5007c5df7d621bd7def3c81b7bb62fb10ea60
CSMate <csiremate@gmail.com>
2024-02-05 13:28:12 +0100

    retrieve user data

--- index.js
+++ index.js
@@ -6,36 +6,62 @@ const querystring = require('querystring')
 const server = http.createServer( (req, res) => {
     res.writeHead( 200, {'Content-Type': 'text/html; charset=utf-8'})
 
-    switch (req.url) {
-        case '/':
-            utilities.getView('homepage', res)
-            return
-            
-       case '/about':
-            utilities.getView('about', res)
-            return
-            
-        case '/contact':
-            utilities.getView('contact', res)
-            return
-            
-        case '/users':
-            fetch('https://jsonplaceholder.typicode.com/users')
+    if ( req.url === '/'){
+        utilities.getView('homepage', res)
+        console.log('Kezdő oldal')
+        return
+    }
+
+    else if (req.url === '/about'){
+        utilities.getView('about', res)
+        console.log('Rólunk')
+        return
+    }
+
+    else if (req.url === '/contact'){
+        utilities.getView('contact', res)
+        console.log('Kapcsolat')
+        return
+    }
+
+    else if (req.url === '/users'){
+        fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(result => {
-
                 const eredmeny = result.map( user => {
                     return `<a href="/users/${user.id}"><li>${user.name}</li></a>`
                 }).join('')
 
+                console.log('Felhasználók:')
                 console.log(eredmeny)
                 utilities.getView('users', res, eredmeny)
                 
             })
             return;
+    }
 
-        default: res.write('Az oldal nem található')
-            break;
+    else if (/^\/users\/([0-9])/.test(req.url) ) {
+        let idpart =  req.url.split('/')
+        const id = idpart[idpart.length-1] || false
+
+        if (id) {
+            fetch('https://jsonplaceholder.typicode.com/users')
+                .then(response => response.json())
+                .then(result => {
+                    const user = result.filter((userrr) => {
+                        return userrr.id == id
+                    })
+                    console.log(user)
+                    console.log(JSON.stringify(user))
+                    console.log('Változó típusa:', typeof(user))
+                    utilities.getView('users', res, JSON.stringify(user))
+                })
+                return
+        }
+    }
+
+    else {
+        res.write('Az oldal nem található')
     }
 
     res.end();
