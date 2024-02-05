08075aa461df37e0c006d03170cf505fd6068707
CSMate <csiremate@gmail.com>
2024-02-05 11:12:56 +0100

    fetch api endpoint and create a list of user names

--- index.js
+++ index.js
@@ -6,32 +6,36 @@ const querystring = require('querystring')
 const server = http.createServer( (req, res) => {
     res.writeHead( 200, {'Content-Type': 'text/html; charset=utf-8'})
 
-    if ( req.url === '/'){
-        utilities.getView('homepage', res)
-        return
-    }
-
-    else if (req.url === '/about'){
-        utilities.getView('about', res)
-        return
-    }
-
-    else if (req.url === '/contact'){
-        utilities.getView('contact', res)
-        return
-    }
-
-    else if (req.url === '/users'){
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.text())
+    switch (req.url) {
+        case '/':
+            utilities.getView('homepage', res)
+            return
+            
+       case '/about':
+            utilities.getView('about', res)
+            return
+            
+        case '/contact':
+            utilities.getView('contact', res)
+            return
+            
+        case '/users':
+            fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
             .then(result => {
-                utilities.getView('users', res, result)
+
+                const eredmeny = result.map( user => {
+                    return `<a href="/users/${user.id}"><li>${user.name}</li></a>`
+                }).join('')
+
+                console.log(eredmeny)
+                utilities.getView('users', res, eredmeny)
+                
             })
             return;
-    }
-    
-    else {
-        res.write('Az oldal nem található')
+
+        default: res.write('Az oldal nem található')
+            break;
     }
 
     res.end();
